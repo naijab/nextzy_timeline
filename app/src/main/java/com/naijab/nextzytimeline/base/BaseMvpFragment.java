8059cc0043d88c2b771e194a20f3df96005e4b0d
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-05-16 11:59:21 +0700

    Edit Base Fragment in Google Code Style

--- app/src/main/java/com/naijab/nextzytimeline/base/BaseMvpFragment.java
+++ app/src/main/java/com/naijab/nextzytimeline/base/BaseMvpFragment.java
@@ -10,94 +10,99 @@ import com.naijab.nextzytimeline.base.exception.MvpNotSetLayoutException;
 import com.naijab.nextzytimeline.base.exception.MvpPresenterNotCreateException;
 
 public abstract class BaseMvpFragment<P extends BaseMvpInterface.Presenter>
-        extends Fragment
-        implements BaseMvpInterface.View{
+    extends Fragment
+    implements BaseMvpInterface.View {
 
-    private P presenter;
+  private P presenter;
 
-    @SuppressWarnings( "unchecked" )
-    @Override
-    public void onCreate( Bundle savedInstanceState ){
-        super.onCreate( savedInstanceState );
-        presenter = createPresenter();
-        presenter.attachView( this );
-        if( savedInstanceState == null ){
-        }else{
-            onRestoreInstanceState( savedInstanceState );
-        }
+  @SuppressWarnings("unchecked")
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    presenter = createPresenter();
+    presenter.attachView(this);
+    if (savedInstanceState == null) {
+    } else {
+      onRestoreInstanceState(savedInstanceState);
     }
+  }
 
-    @Nullable
-    @Override
-    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ){
-        int layoutResId = getLayoutView();
-        if( getLayoutView() == 0 ) throw new MvpNotSetLayoutException();
-        return inflater.inflate( layoutResId, container, false );
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    int layoutResId = getLayoutView();
+    if (getLayoutView() == 0) {
+      throw new MvpNotSetLayoutException();
     }
+    return inflater.inflate(layoutResId, container, false);
+  }
 
-    @Override
-    public void onViewCreated( View view, @Nullable Bundle savedInstanceState ){
-        super.onViewCreated( view, savedInstanceState );
-        bindView( view );
-        setupInstance();
-        setupView();
-        getPresenter().onViewCreate();
-        if( savedInstanceState == null ){
-            initialize();
-        }else{
-            restoreView( savedInstanceState );
-        }
+  @Override
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    bindView(view);
+    setupInstance();
+    setupView();
+    getPresenter().onViewCreate();
+    if (savedInstanceState == null) {
+      initialize();
+    } else {
+      restoreView(savedInstanceState);
     }
+  }
 
 
-    @Override
-    public void onStart(){
-        super.onStart();
-        getPresenter().onViewStart();
+  @Override
+  public void onStart() {
+    super.onStart();
+    getPresenter().onViewStart();
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    getPresenter().onViewStop();
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    getPresenter().onViewDestroy();
+    presenter.detachView();
+  }
+
+
+  @Override
+  public P getPresenter() {
+    if (presenter != null) {
+      return presenter;
     }
+    throw new MvpPresenterNotCreateException();
+  }
 
-    @Override
-    public void onStop(){
-        super.onStop();
-        getPresenter().onViewStop();
-    }
+  @Override
+  public void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+  }
 
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
-        getPresenter().onViewDestroy();
-        presenter.detachView();
-    }
+  public void onRestoreInstanceState(Bundle savedInstanceState) {
+  }
 
+  public abstract P createPresenter();
 
-    @Override
-    public P getPresenter(){
-        if( presenter != null ) return presenter;
-        throw new MvpPresenterNotCreateException();
-    }
+  public abstract int getLayoutView();
 
-    @Override
-    public void onSaveInstanceState( Bundle outState ){
-        super.onSaveInstanceState( outState );
-    }
+  public abstract void bindView(View view);
 
-    public void onRestoreInstanceState( Bundle savedInstanceState ){
-    }
+  public abstract void setupInstance();
 
-    public abstract P createPresenter();
+  public abstract void setupView();
 
-    public abstract int getLayoutView();
+  public abstract void initialize();
 
-    public abstract void bindView( View view );
-
-    public abstract void setupInstance();
-
-    public abstract void setupView();
-
-    public abstract void initialize();
-
-    public void restoreView( Bundle savedInstanceState ){
-    }
+  public void restoreView(Bundle savedInstanceState) {
+  }
 
 }
 
