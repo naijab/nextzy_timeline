f78ebec3b38a782e46ad14622e54a8ecd4ada400
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-05-16 17:38:32 +0700

    Add String Utility

---
+++ app/src/main/java/com/naijab/nextzytimeline/utility/StringUtility.java
@@ -0,0 +1,24 @@
+package com.naijab.nextzytimeline.utility;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import com.naijab.nextzytimeline.R;
+
+public class StringUtility {
+
+  @NonNull
+  public static String getHomeString(Context context){
+    return context.getResources().getString(R.string.tab_home);
+  }
+
+  @NonNull
+  public static String getAboutString(Context context){
+    return context.getResources().getString(R.string.tab_about);
+  }
+
+  @NonNull
+  public static String getContactString(Context context){
+    return context.getResources().getString(R.string.tab_contact);
+  }
+
+}
