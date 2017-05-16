1a22be03a3e4b86a883062a23481e7b97c004c0a
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-05-16 14:38:28 +0700

    Edit Splash add Handler post delay to MainActivity

--- app/src/main/java/com/naijab/nextzytimeline/ui/SplashScreenActivity.java
+++ app/src/main/java/com/naijab/nextzytimeline/ui/SplashScreenActivity.java
@@ -1,49 +1,70 @@
 package com.naijab.nextzytimeline.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import com.naijab.nextzytimeline.R;
 import com.naijab.nextzytimeline.base.BaseMvpActivity;
+import com.naijab.nextzytimeline.ui.Main.MainActivity;
 import com.naijab.nextzytimeline.ui.SplashScreenActivityInterface.Presenter;
 
 public class SplashScreenActivity extends BaseMvpActivity<Presenter>
-        implements SplashScreenActivityInterface.View{
+    implements SplashScreenActivityInterface.View {
 
+  private static final long DELAY_TIME = 1000;
 
-    @Override
-    public Presenter createPresenter(){
-        return SplashScreenActivityPresenter.create();
-    }
+  @Override
+  public Presenter createPresenter() {
+    return SplashScreenActivityPresenter.create();
+  }
 
-    @Override
-    public int getLayoutView(){
-        return 0;
-    }
+  @Override
+  public int getLayoutView() {
+    return R.layout.splash_screen;
+  }
 
+  @Override
+  public void bindView() {
+  }
 
-    @Override
-    public void bindView(){
-    }
+  @Override
+  public void setupInstance() {
+    goToMainActivity();
+  }
 
-    @Override
-    public void setupInstance(){
+  @Override
+  public void setupView() {
+  }
 
-    }
+  @Override
+  public void initialize() {
+  }
 
-    @Override
-    public void setupView(){
-    }
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+  }
 
-    @Override
-    public void initialize(){
-    }
+  @Override
+  protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+  }
 
-    @Override
-    protected void onSaveInstanceState( Bundle outState ){
-        super.onSaveInstanceState( outState );
-    }
+  private void delayHandler(Runnable rannable){
+    Handler handler = new Handler();
+    handler.postDelayed(rannable, DELAY_TIME);
+  }
 
-    @Override
-    protected void onRestoreInstanceState( Bundle savedInstanceState ){
-        super.onRestoreInstanceState( savedInstanceState );
-    }
+  private void goToMainActivity() {
+    delayHandler(new Runnable() {
+      @Override
+      public void run() {
+        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+        startActivity( intent );
+        finish();
+        overridePendingTransition( R.anim.fade_in, R.anim.fade_out );
+      }
+    });
+  }
 }
 
