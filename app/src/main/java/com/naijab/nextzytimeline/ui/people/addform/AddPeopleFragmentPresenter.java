236ab6d0d220206f728ffb5fc24825e46125b96c
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-05-19 14:54:48 +0700

    Add Realm AddPeople Presenter

--- app/src/main/java/com/naijab/nextzytimeline/ui/people/addform/AddPeopleFragmentPresenter.java
+++ app/src/main/java/com/naijab/nextzytimeline/ui/people/addform/AddPeopleFragmentPresenter.java
@@ -2,11 +2,26 @@ package com.naijab.nextzytimeline.ui.people.addform;
 
 import com.naijab.nextzytimeline.base.BaseMvpPresenter;
 
+import io.realm.Realm;
+
 public class AddPeopleFragmentPresenter extends BaseMvpPresenter<AddPeopleFragmentInterface.View>
         implements AddPeopleFragmentInterface.Presenter{
+
+    private Realm realm;
 
     public static AddPeopleFragmentInterface.Presenter create(){
         return new AddPeopleFragmentPresenter();
     }
 
+    @Override
+    public void onViewStart() {
+        super.onViewStart();
+        realm = Realm.getDefaultInstance();
+    }
+
+    @Override
+    public void onViewStop() {
+        super.onViewStop();
+        realm.close();
+    }
 }
