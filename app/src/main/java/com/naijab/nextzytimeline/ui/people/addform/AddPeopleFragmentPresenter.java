b722f22b83e11a780ae67bf23a000060fb3b1d2d
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-05-22 16:28:52 +0700

    Add  Insert Realm in Presenter

--- app/src/main/java/com/naijab/nextzytimeline/ui/people/addform/AddPeopleFragmentPresenter.java
+++ app/src/main/java/com/naijab/nextzytimeline/ui/people/addform/AddPeopleFragmentPresenter.java
@@ -1,15 +1,17 @@
 package com.naijab.nextzytimeline.ui.people.addform;
 
 import com.naijab.nextzytimeline.base.BaseMvpPresenter;
+import com.naijab.nextzytimeline.ui.people.model.PeopleModel;
 
 import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class AddPeopleFragmentPresenter extends BaseMvpPresenter<AddPeopleFragmentInterface.View>
-        implements AddPeopleFragmentInterface.Presenter{
+        implements AddPeopleFragmentInterface.Presenter {
 
     private Realm realm;
 
-    public static AddPeopleFragmentInterface.Presenter create(){
+    public static AddPeopleFragmentInterface.Presenter create() {
         return new AddPeopleFragmentPresenter();
     }
 
@@ -23,5 +25,46 @@ public class AddPeopleFragmentPresenter extends BaseMvpPresenter<AddPeopleFragme
     public void onViewStop() {
         super.onViewStop();
         realm.close();
+    }
+
+    @Override
+    public void saveIntoRealm(final String name,
+                              final String birthday,
+                              final String startJob,
+                              final String job,
+                              final String game,
+                              final String smartphone,
+                              final String photos) {
+
+        RealmResults<PeopleModel> resultRealm = realm.where(PeopleModel.class)
+                .findAll();
+        final int id = resultRealm.size() + 1;
+
+        realm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm bgRealm) {
+                PeopleModel people = realm.createObject(PeopleModel.class, id);
+                people.setName(name);
+                people.setBirthday(birthday);
+                people.setJobstart(startJob);
+                people.setJob(job);
+                people.setGame(game);
+                people.setSmartphone(smartphone);
+                people.setPhoto(photos);
+            }
+        }, new Realm.Transaction.OnSuccess() {
+            @Override
+            public void onSuccess() {
+                getView().response("onSuccess");
+            }
+        }, new Realm.Transaction.OnError() {
+            @Override
+            public void onError(Throwable error) {
+                getView().response(error.getMessage());
+            }
+        });
+
+
+
     }
 }
