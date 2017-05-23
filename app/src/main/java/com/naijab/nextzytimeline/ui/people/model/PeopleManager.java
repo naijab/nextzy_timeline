54e0aae176d5b0e2a59a63a4b21721bc15448afc
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-05-23 14:11:23 +0700

    Edit PeopleManager

--- app/src/main/java/com/naijab/nextzytimeline/ui/people/model/PeopleManager.java
+++ app/src/main/java/com/naijab/nextzytimeline/ui/people/model/PeopleManager.java
@@ -1,55 +1,27 @@
 package com.naijab.nextzytimeline.ui.people.model;
 
-import android.app.Activity;
-import android.app.Application;
-import android.support.v4.app.Fragment;
-
 import io.realm.Realm;
 import io.realm.RealmResults;
 
 public class PeopleManager {
 
     private static PeopleManager peopleManager;
-    private final Realm realm;
-
-    public PeopleManager(Application application) {
-        realm = Realm.getDefaultInstance();
-    }
-
-    public static PeopleManager with(Fragment fragment) {
-
-        if (peopleManager == null) {
-            peopleManager = new PeopleManager(fragment.getActivity().getApplication());
-        }
-        return peopleManager;
-    }
-
-    public static PeopleManager with(Activity activity) {
-
-        if (peopleManager == null) {
-            peopleManager = new PeopleManager(activity.getApplication());
-        }
-        return peopleManager;
-    }
-
-    public static PeopleManager with(Application application) {
-
-        if (peopleManager == null) {
-            peopleManager = new PeopleManager(application);
-        }
-        return peopleManager;
-    }
+    private Realm realm = Realm.getDefaultInstance();
 
     public static PeopleManager getInstance() {
+        if (peopleManager == null) {
+            peopleManager = new PeopleManager();
+        }
         return peopleManager;
     }
 
-    public Realm getRealm() {
+    public Realm getRealm(){
         return realm;
     }
 
     public RealmResults<PeopleModel> getPeople() {
-        return realm.where(PeopleModel.class).findAll();
+        return realm.where(PeopleModel.class)
+                .findAll();
     }
 
     public PeopleModel getPeople(String id) {
