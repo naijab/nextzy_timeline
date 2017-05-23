a0fc81c6b2a0a53ee96339a83035c72fd0c1c2ce
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-05-23 15:23:16 +0700

    Add getDeleteSuccess String

--- app/src/main/java/com/naijab/nextzytimeline/utility/StringUtility.java
+++ app/src/main/java/com/naijab/nextzytimeline/utility/StringUtility.java
@@ -2,28 +2,34 @@ package com.naijab.nextzytimeline.utility;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+
 import com.naijab.nextzytimeline.R;
 
 public class StringUtility {
 
-  @NonNull
-  public static String getHomeString(Context context){
-    return context.getResources().getString(R.string.tab_home);
-  }
+    @NonNull
+    public static String getHomeString(Context context) {
+        return context.getResources().getString(R.string.tab_home);
+    }
 
-  @NonNull
-  public static String getAboutString(Context context){
-    return context.getResources().getString(R.string.tab_about);
-  }
+    @NonNull
+    public static String getAboutString(Context context) {
+        return context.getResources().getString(R.string.tab_about);
+    }
 
-  @NonNull
-  public static String getContactString(Context context){
-    return context.getResources().getString(R.string.tab_contact);
-  }
+    @NonNull
+    public static String getContactString(Context context) {
+        return context.getResources().getString(R.string.tab_contact);
+    }
 
-  @NonNull
-  public static String getSaveSuccess(Context context){
-    return context.getResources().getString(R.string.save_success);
-  }
+    @NonNull
+    public static String getSaveSuccess(Context context) {
+        return context.getResources().getString(R.string.save_success);
+    }
+
+    @NonNull
+    public static String getDeleteSuccess(Context context){
+        return context.getResources().getString(R.string.save_success);
+    }
 
 }
