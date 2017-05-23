91077cd520040b5a591987d893d6ab75462cf742
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-05-23 14:37:03 +0700

    Edit Presenter saveIntoRealm

--- app/src/main/java/com/naijab/nextzytimeline/ui/people/addform/AddPeopleFragmentPresenter.java
+++ app/src/main/java/com/naijab/nextzytimeline/ui/people/addform/AddPeopleFragmentPresenter.java
@@ -1,17 +1,17 @@
 package com.naijab.nextzytimeline.ui.people.addform;
 
+import android.content.Context;
+
 import com.naijab.nextzytimeline.base.BaseMvpPresenter;
+import com.naijab.nextzytimeline.ui.people.model.PeopleManager;
 import com.naijab.nextzytimeline.ui.people.model.PeopleModel;
 
 import io.realm.Realm;
-import io.realm.RealmResults;
 
 public class AddPeopleFragmentPresenter extends BaseMvpPresenter<AddPeopleFragmentInterface.View>
         implements AddPeopleFragmentInterface.Presenter {
 
     private Realm realm;
-    private RealmResults<PeopleModel> resultRealm;
-    int id = 0;
 
     public static AddPeopleFragmentInterface.Presenter create() {
         return new AddPeopleFragmentPresenter();
@@ -21,6 +21,10 @@ public class AddPeopleFragmentPresenter extends BaseMvpPresenter<AddPeopleFragme
     public void onViewStart() {
         super.onViewStart();
         realm = Realm.getDefaultInstance();
+        String name = PeopleManager.getInstance().getPeople().get(4).getName();
+        String job = PeopleManager.getInstance().getPeople().get(4).getJob();
+        String photos = PeopleManager.getInstance().getPeople().get(4).getPhoto();
+        getView().response(name + " " + job + " " + photos);
     }
 
     @Override
@@ -31,37 +35,16 @@ public class AddPeopleFragmentPresenter extends BaseMvpPresenter<AddPeopleFragme
 
 
     @Override
-    public void saveIntoRealm(final PeopleModel peopleModel) {
-
-        resultRealm = realm.where(PeopleModel.class)
-                .findAll();
-        id = resultRealm.size() + 1;
-
-        Realm realm = Realm.getDefaultInstance();
-        realm.executeTransactionAsync(new Realm.Transaction() {
+    public void saveIntoRealm(PeopleModel peopleModel, Context context) {
+        PeopleManager.getInstance().saveRealm(peopleModel, context, new PeopleManager.SaveCallback() {
             @Override
-            public void execute(Realm bgRealm) {
-
-                PeopleModel people = bgRealm.createObject(PeopleModel.class, id);
-                people.setName(peopleModel.getName());
-                people.setBirthday(peopleModel.getBirthday());
-                people.setJobstart(peopleModel.getJobstart());
-                people.setJob(peopleModel.getJob());
-                people.setGame(peopleModel.getGame());
-                people.setSmartphone(peopleModel.getSmartphone());
-                people.setPhoto(peopleModel.getPhoto());
+            public void onSaveSuccess(String message) {
+                getView().response(message);
             }
-        }, new Realm.Transaction.OnSuccess() {
+
             @Override
-            public void onSuccess() {
-                getView().response("onSuccess");
-                getView().saveIsFinish(true);
-            }
-        }, new Realm.Transaction.OnError() {
-            @Override
-            public void onError(Throwable error) {
-                getView().response(error.getMessage());
-                getView().saveIsFinish(false);
+            public void onSaveError(String message) {
+                getView().response(message);
             }
         });
     }
