5d5c5073061fc9a853ced1b0915cf819ccc65dba
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-05-26 18:42:01 +0700

    Edit PeopleManager

--- app/src/main/java/com/naijab/nextzytimeline/ui/people/model/PeopleManager.java
+++ app/src/main/java/com/naijab/nextzytimeline/ui/people/model/PeopleManager.java
@@ -10,22 +10,28 @@ import io.realm.RealmResults;
 public class PeopleManager {
 
     private static PeopleManager peopleManager;
-    private Realm realm = Realm.getDefaultInstance();
     private RealmResults<PeopleModel> resultRealm;
+    private PeopleModel peopleItem;
+    private Realm realm = Realm.getDefaultInstance();
     private int id = 0;
 
-    public static PeopleManager getInstance() {
+    public PeopleManager(Realm realm) {
+        this.realm = realm;
+    }
+
+    public static PeopleManager getInstance(Realm realm) {
         if (peopleManager == null) {
-            peopleManager = new PeopleManager();
+            peopleManager = new PeopleManager(realm);
         }
         return peopleManager;
     }
 
-    public Realm getRealm(){
+    public Realm getRealm() {
         return realm;
     }
 
     public RealmResults<PeopleModel> getPeople() {
+        realm = Realm.getDefaultInstance();
         return realm.where(PeopleModel.class)
                 .findAll();
     }
@@ -36,7 +42,7 @@ public class PeopleManager {
                 .findFirst();
     }
 
-    public void deleteAll(final Context context, final onDeleteCallBack callBack){
+    public void deleteAll(final Context context, final onDeleteCallBack callBack) {
         realm.executeTransactionAsync(new Realm.Transaction() {
             @Override
             public void execute(Realm realm) {
@@ -56,9 +62,34 @@ public class PeopleManager {
 
     }
 
+    public void deleteByID(final int id,
+                           final Context context,
+                           final onDeleteCallBack callBack) {
+        realm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm bgRealm) {
+                RealmResults<PeopleModel> results = bgRealm.where(PeopleModel.class)
+                        .equalTo("id", id)
+                        .findAll();
+                results.deleteAllFromRealm();
+            }
+        }, new Realm.Transaction.OnSuccess() {
+            @Override
+            public void onSuccess() {
+                callBack.onDeleteSuccess(StringUtility.getDeleteSuccess(context));
+            }
+        }, new Realm.Transaction.OnError() {
+            @Override
+            public void onError(Throwable error) {
+                callBack.onDeleteError(error.getMessage());
+            }
+        });
+
+    }
+
     public void saveRealm(final PeopleModel peopleModel,
                           final Context context,
-                          final onSaveCallBack callBack){
+                          final onSaveCallBack callBack) {
 
         resultRealm = realm.where(PeopleModel.class)
                 .findAll();
@@ -95,13 +126,65 @@ public class PeopleManager {
         });
     }
 
+
+    public void editRealm(final PeopleModel peopleModel,
+                          final Context context,
+                          final onEditCallBack callBack) {
+
+
+        realm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm bgRealm) {
+
+                RealmResults<PeopleModel> results = bgRealm.where(PeopleModel.class)
+                        .findAll();
+
+                peopleItem = results.where().equalTo("id", peopleModel.getId())
+                        .findFirst();
+
+                peopleItem.setName(peopleModel.getName());
+                peopleItem.setJob(peopleModel.getJob());
+                peopleItem.setBirthDay(peopleModel.getBirthDay());
+                peopleItem.setJobStart(peopleModel.getJobStart());
+                peopleItem.setJobDescription(peopleModel.getJobDescription());
+                peopleItem.setGame(peopleModel.getGame());
+                peopleItem.setSmartPhone(peopleModel.getSmartPhone());
+                peopleItem.setPhoto(peopleModel.getPhoto());
+                peopleItem.setProfile(peopleModel.getProfile());
+
+                bgRealm.copyToRealmOrUpdate(peopleItem);
+
+            }
+        }, new Realm.Transaction.OnSuccess() {
+            @Override
+            public void onSuccess() {
+                callBack.onSaveSuccess(StringUtility.getSaveSuccess(context));
+                realm.close();
+            }
+        }, new Realm.Transaction.OnError() {
+            @Override
+            public void onError(Throwable error) {
+                callBack.onSaveError(error.getMessage());
+            }
+        });
+    }
+
+
     public interface onSaveCallBack {
         void onSaveSuccess(String message);
+
+        void onSaveError(String message);
+    }
+
+    public interface onEditCallBack {
+        void onSaveSuccess(String message);
+
         void onSaveError(String message);
     }
 
     public interface onDeleteCallBack {
         void onDeleteSuccess(String message);
+
         void onDeleteError(String message);
     }
 
