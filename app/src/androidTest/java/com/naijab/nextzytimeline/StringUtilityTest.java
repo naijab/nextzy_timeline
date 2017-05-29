42eca9274058d27302dc9f7670f43b8d429cb52e
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-05-29 15:20:42 +0700

    Edit Test StringUtility

--- app/src/androidTest/java/com/naijab/nextzytimeline/StringUtilityTest.java
+++ app/src/androidTest/java/com/naijab/nextzytimeline/StringUtilityTest.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.naijab.nextzytimeline.ui.people.model.PeopleManager;
 import com.naijab.nextzytimeline.utility.StringUtility;
 
 import junit.framework.Assert;
@@ -13,25 +12,19 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-/**
- * Created by Xiltron on 28/5/2560.
- */
 @RunWith(AndroidJUnit4.class)
 public class StringUtilityTest {
 
-    Context context;
-
     @Before
     public void initContext(){
-        context = InstrumentationRegistry.getTargetContext();
     }
 
     @Test
     public void getString(){
-        String success = StringUtility.getSaveSuccess(context);
-        String successError = StringUtility.getSaveError(context);
-        String delete = StringUtility.getDeleteSuccess(context);
-        String deleteError = StringUtility.getDeleteError(context);
+        String success = StringUtility.getSaveSuccess();
+        String successError = StringUtility.getSaveError();
+        String delete = StringUtility.getDeleteSuccess();
+        String deleteError = StringUtility.getDeleteError();
         Assert.assertNotNull(success);
         Assert.assertNotNull(successError);
         Assert.assertNotNull(delete);
