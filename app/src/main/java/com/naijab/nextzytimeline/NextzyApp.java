7ff980c328ec47f78fe1301cfd666651d2558684
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-06-02 16:31:38 +0700

    Add Context

--- app/src/main/java/com/naijab/nextzytimeline/NextzyApp.java
+++ app/src/main/java/com/naijab/nextzytimeline/NextzyApp.java
@@ -1,18 +1,25 @@
 package com.naijab.nextzytimeline;
 
 import android.app.Application;
+import android.content.Context;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
 public class NextzyApp extends Application{
 
+    private static Context mContext;
+
     @Override
     public void onCreate() {
         super.onCreate();
-
+        mContext = this;
         initRealm();
 
+    }
+
+    public static Context getContext(){
+        return mContext;
     }
 
     private void initRealm() {
