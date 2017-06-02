ce2027e00f3d66f262ed633433c427f3d3893e40
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-06-02 17:14:05 +0700

    rename variable

--- app/src/main/java/com/naijab/nextzytimeline/ui/people/addform/fragment/AddPeopleFragment.java
+++ app/src/main/java/com/naijab/nextzytimeline/ui/people/addform/fragment/AddPeopleFragment.java
@@ -34,8 +34,8 @@ import static android.app.Activity.RESULT_OK;
 public class AddPeopleFragment extends BaseMvpFragment<AddPeopleFragmentInterface.Presenter>
         implements AddPeopleFragmentInterface.View {
 
-    private EditText nameAndLastName, job, dateBirth, dateJob, jobDescription, game, smartPhone;
-    private ImageView profile, photo;
+    private EditText edtName, edtJob, edtDateBirth, edtDateJob, edtJobDescription, edtGame, edtSmartPhone;
+    private ImageView ivProfile, ivPhoto;
     private FloatingActionButton fabPhoto;
     private Uri uriProfile, uriPhoto;
     private String nameS, dateBirthS, dateJobS, jobS, jobDescriptionS, gameS, smartPhoneS, photoS, profileS;
@@ -67,15 +67,15 @@ public class AddPeopleFragment extends BaseMvpFragment<AddPeopleFragmentInterfac
 
     @Override
     public void bindView(View view) {
-        nameAndLastName = (EditText) view.findViewById(R.id.edt_name_and_lastname);
-        job = (EditText) view.findViewById(R.id.edt_job);
-        dateBirth = (EditText) view.findViewById(R.id.edt_birthday);
-        dateJob = (EditText) view.findViewById(R.id.edt_startjob);
-        jobDescription = (EditText) view.findViewById(R.id.edt_job_description);
-        game = (EditText) view.findViewById(R.id.edt_game);
-        smartPhone = (EditText) view.findViewById(R.id.edt_your_phone);
-        photo = (ImageView) view.findViewById(R.id.iv_photo);
-        profile = (ImageView) view.findViewById(R.id.iv_profile);
+        edtName = (EditText) view.findViewById(R.id.edt_name_and_lastname);
+        edtJob = (EditText) view.findViewById(R.id.edt_job);
+        edtDateBirth = (EditText) view.findViewById(R.id.edt_birthday);
+        edtDateJob = (EditText) view.findViewById(R.id.edt_startjob);
+        edtJobDescription = (EditText) view.findViewById(R.id.edt_job_description);
+        edtGame = (EditText) view.findViewById(R.id.edt_game);
+        edtSmartPhone = (EditText) view.findViewById(R.id.edt_your_phone);
+        ivPhoto = (ImageView) view.findViewById(R.id.iv_photo);
+        ivProfile = (ImageView) view.findViewById(R.id.iv_profile);
         fabPhoto = (FloatingActionButton) view.findViewById(R.id.fab);
     }
 
@@ -86,9 +86,9 @@ public class AddPeopleFragment extends BaseMvpFragment<AddPeopleFragmentInterfac
 
     @Override
     public void setupView() {
-        dateBirth.setOnClickListener(showDatePicker);
-        dateJob.setOnClickListener(showDatePicker);
-        profile.setOnClickListener(takeProfile);
+        edtDateBirth.setOnClickListener(showDatePicker);
+        edtDateJob.setOnClickListener(showDatePicker);
+        ivProfile.setOnClickListener(takeProfile);
         fabPhoto.setOnClickListener(takePhoto);
     }
 
@@ -132,13 +132,13 @@ public class AddPeopleFragment extends BaseMvpFragment<AddPeopleFragmentInterfac
     }
 
     private boolean checkIsEmpty() {
-        if (nameAndLastName != null &&
-                dateBirth != null &&
-                dateJob != null &&
-                job != null &&
-                jobDescription != null &&
-                game != null &&
-                smartPhone != null &&
+        if (edtName != null &&
+                edtDateBirth != null &&
+                edtDateJob != null &&
+                edtJob != null &&
+                edtJobDescription != null &&
+                edtGame != null &&
+                edtSmartPhone != null &&
                 uriProfile != null &&
                 uriPhoto != null) {
             return true;
@@ -147,13 +147,13 @@ public class AddPeopleFragment extends BaseMvpFragment<AddPeopleFragmentInterfac
     }
 
     private void saveToRealm() {
-        nameS = nameAndLastName.getText().toString();
-        dateBirthS = dateBirth.getText().toString();
-        dateJobS = dateJob.getText().toString();
-        jobS = job.getText().toString();
-        jobDescriptionS = jobDescription.getText().toString();
-        gameS = game.getText().toString();
-        smartPhoneS = smartPhone.getText().toString();
+        nameS = edtName.getText().toString();
+        dateBirthS = edtDateBirth.getText().toString();
+        dateJobS = edtDateJob.getText().toString();
+        jobS = edtJob.getText().toString();
+        jobDescriptionS = edtJobDescription.getText().toString();
+        gameS = edtGame.getText().toString();
+        smartPhoneS = edtSmartPhone.getText().toString();
         profileS = uriProfile.toString();
         photoS = uriPhoto.toString();
 
@@ -239,7 +239,7 @@ public class AddPeopleFragment extends BaseMvpFragment<AddPeopleFragmentInterfac
             ContentResolver contentResolver = getActivity().getContentResolver();
             try {
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uriProfile);
-                profile.setImageBitmap(bitmap);
+                ivProfile.setImageBitmap(bitmap);
 
                 Log.i("AddPeople", "Image: " + uriProfile.toString());
 
@@ -252,7 +252,7 @@ public class AddPeopleFragment extends BaseMvpFragment<AddPeopleFragmentInterfac
             ContentResolver contentResolver = getActivity().getContentResolver();
             try {
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uriPhoto);
-                photo.setImageBitmap(bitmap);
+                ivPhoto.setImageBitmap(bitmap);
 
                 Log.i("AddPeople", "Image: " + uriPhoto.toString());
 
@@ -267,13 +267,13 @@ public class AddPeopleFragment extends BaseMvpFragment<AddPeopleFragmentInterfac
     private void setDateBirth(int year,
                               int monthOfYear,
                               int dayOfMonth) {
-        dateBirth.setText(dayOfMonth + "-" + monthOfYear + "-" + year);
+        edtDateBirth.setText(dayOfMonth + "-" + monthOfYear + "-" + year);
     }
 
     private void setDateJob(int year,
                             int monthOfYear,
                             int dayOfMonth) {
-        dateJob.setText(dayOfMonth + "-" + monthOfYear + "-" + year);
+        edtDateJob.setText(dayOfMonth + "-" + monthOfYear + "-" + year);
     }
 
     @Override
