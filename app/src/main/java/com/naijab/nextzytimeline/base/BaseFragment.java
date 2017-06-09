d6ebd5b0255d0d26679afe5bf5901779b23c678d
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-06-09 10:39:54 +0700

    Add Base Fragment

--- app/src/main/java/com/naijab/nextzytimeline/base/BaseFragment.java
+++ app/src/main/java/com/naijab/nextzytimeline/base/BaseFragment.java
@@ -1,4 +1,67 @@
 package com.naijab.nextzytimeline.base;
 
-public abstract class BaseFragment {
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+public abstract class BaseFragment extends Fragment{
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState == null) {
+        } else {
+            onRestoreInstanceState(savedInstanceState);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        int layoutResId = getLayoutView();
+        return inflater.inflate(layoutResId, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        bindView(view);
+        setupInstance();
+        setupView();
+        if (savedInstanceState == null) {
+            initialize();
+        }
+    }
+
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+    }
+
+    public abstract int getLayoutView();
+
+    public abstract void bindView(View view);
+
+    public abstract void setupInstance();
+
+    public abstract void setupView();
+
+    public abstract void initialize();
+
+    public void hideKeyboard(){
+        View view = this.getActivity().getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    public void showToast(String message){
+        Toast.makeText(getActivity(), "" + message, Toast.LENGTH_SHORT).show();
+    }
 }
