2051ed98253238d165de04d0355cbe2369f5d44f
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-06-09 10:28:31 +0700

    Edit BaseActivity

--- app/src/main/java/com/naijab/nextzytimeline/base/BaseActivity.java
+++ app/src/main/java/com/naijab/nextzytimeline/base/BaseActivity.java
@@ -1,4 +1,33 @@
 package com.naijab.nextzytimeline.base;
 
-public class BaseActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.akexorcist.localizationactivity.LocalizationActivity;
+
+public abstract class BaseActivity extends LocalizationActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int layoutResId = getLayoutView();
+        setContentView(layoutResId);
+        bindView();
+        setupInstance();
+        setupView();
+        if( savedInstanceState == null ){
+            initialize();
+        }
+    }
+
+    public abstract int getLayoutView();
+
+    public abstract void bindView();
+
+    public abstract void setupInstance();
+
+    public abstract void setupView();
+
+    public abstract void initialize();
+
 }
